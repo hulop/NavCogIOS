3d3e8824b917e9b28c5570b3c439a183ccc000fd
Daisuke Sato <dsato@jp.ibm.com>
2016-01-08 18:15:54 +0900

    fix for blelocpp

--- NavCog/Model/Localization/OneDLocalizer.mm
+++ NavCog/Model/Localization/OneDLocalizer.mm
@@ -24,7 +24,6 @@
 
 #import <bleloc/PoseRandomWalker.hpp>
 
-#import <bleloc/ObservationModelStub.hpp>
 #import <bleloc/GridResampler.hpp>
 #import <bleloc/StatusInitializerStub.hpp>
 #import <bleloc/StatusInitializerImpl.hpp>
@@ -173,8 +172,8 @@ void d1calledWhenUpdated(Status * pStatus){
     // Instantiate sensor data processors
     // Orientation
     OrientationMeterAverageParameters orientationMeterAverageParameters;
-    orientationMeterAverageParameters.setInterval(0.1);
-    orientationMeterAverageParameters.setWindowAveraging(0.1);
+    orientationMeterAverageParameters.interval(0.1);
+    orientationMeterAverageParameters.windowAveraging(0.1);
     std::shared_ptr<OrientationMeter> orientationMeter(new OrientationMeterAverage(orientationMeterAverageParameters));
     
     self.orientationMeter = orientationMeter;
@@ -182,7 +181,7 @@ void d1calledWhenUpdated(Status * pStatus){
     // Pedometer
     PedometerWalkingStateParameters pedometerWSParams;
     // TODO
-    pedometerWSParams.updatePeriod = 0.1;
+    pedometerWSParams.updatePeriod(0.1);
     // END TODO
     std::shared_ptr<Pedometer> pedometer(new PedometerWalkingState(pedometerWSParams));
     
@@ -194,26 +193,26 @@ void d1calledWhenUpdated(Status * pStatus){
     // TODO (PoseProperty and StateProperty)
     PoseProperty poseProperty;
     StateProperty stateProperty;
-    poseProperty.meanVelocity = 1.0;
-    poseProperty.stdVelocity = 0.3;
-    poseProperty.driftVelocity = 0.1;
-    poseProperty.minVelocity = 0.1;
-    poseProperty.maxVelocity = 1.5;
-    poseProperty.stdOrientation = 3.0/180.0*M_PI;
+    poseProperty.meanVelocity(1.0);
+    poseProperty.stdVelocity(0.3);
+    poseProperty.diffusionVelocity(0.1);
+    poseProperty.minVelocity(0.1);
+    poseProperty.maxVelocity(1.5);
+    poseProperty.stdOrientation(3.0/180.0*M_PI);
     
-    //stateProperty.meanRssiBias = -4.0;
-    stateProperty.meanRssiBias = 0.0;
-    stateProperty.stdRssiBias = 0.2;
-    stateProperty.driftRssiBias = 0.2;
-    stateProperty.driftOrientationBias = 1.0/180*M_PI;
+    //stateProperty.meanRssiBias(-4.0);
+    stateProperty.meanRssiBias(0.0);
+    stateProperty.stdRssiBias(0.2);
+    stateProperty.diffusionRssiBias(0.2);
+    stateProperty.diffusionOrientationBias(1.0/180*M_PI);
     // END TODO
     
     // Build poseRandomWalker
     PoseRandomWalkerProperty poseRandomWalkerProperty;
     std::shared_ptr<PoseRandomWalker>poseRandomWalker(new PoseRandomWalker());
-    poseRandomWalkerProperty.pOrientationMeter = orientationMeter.get();
-    poseRandomWalkerProperty.pPedomter = pedometer.get();
-    poseRandomWalkerProperty.angularVelocityLimit = 30.0/180.0*M_PI;
+    poseRandomWalkerProperty.orientationMeter(orientationMeter.get());
+    poseRandomWalkerProperty.pedometer(pedometer.get());
+    poseRandomWalkerProperty.angularVelocityLimit(30.0/180.0*M_PI);
     poseRandomWalker->setProperty(poseRandomWalkerProperty);
     poseRandomWalker->setPoseProperty(poseProperty);
     poseRandomWalker->setStateProperty(stateProperty);
@@ -221,8 +220,8 @@ void d1calledWhenUpdated(Status * pStatus){
     // Combine poseRandomWalker and building
     PoseRandomWalkerInBuildingProperty prwBuildingProperty;
     // TODO
-    prwBuildingProperty.maxIncidenceAngle = 45.0/180.0*M_PI;
-    prwBuildingProperty.weightDecayRate = 0.9;
+    prwBuildingProperty.maxIncidenceAngle(45.0/180.0*M_PI);
+    prwBuildingProperty.weightDecayRate(0.9);
     // END TODO
     Building building = _dataStore->getBuilding();
     std::shared_ptr<PoseRandomWalkerInBuilding> poseRandomWalkerInBuilding(new PoseRandomWalkerInBuilding());
